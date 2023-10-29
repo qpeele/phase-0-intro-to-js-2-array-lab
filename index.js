b8b234a7a7c6044ef67aff465407f6b826a2ca43
Quachell Peele <quachellpeele@gmail.com>
2023-10-29 16:27:22 -0400

    arrays are hard

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Write your solution here!
+let cats = ["Milo", "Otis","Garfield"]
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name)
+}
+
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+}
+
+
+function appendCat(name) {
+    // Create a new array by concatenating the existing `cats` array with the new cat
+    const newCats = cats.concat(name);
+  
+    // Return the new array
+    return newCats;
+  }
+  function prependCat(name) {
+    // Create a new array by concatenating the new cat with the existing `cats` array
+    const newCats = [name, ...cats];
+  
+    // Return the new array
+    return newCats;
+  }
+
+
+  function removeLastCat() {
+    // Create a new array by slicing the `cats` array, excluding the last element
+    const newCats = cats.slice(0, -1);
+  
+    // Return the new array
+    return newCats;
+  }
+  
+  function removeFirstCat() {
+    // Create a new array by slicing the `cats` array, excluding the first element
+    const newCats = cats.slice(1);
+  
+    // Return the new array
+    return newCats;
+  } 
